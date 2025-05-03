ec1bdfab7f61536e94f53d6c3d622d8bc86eaefe
ysb <vahyeon2503@sonline20.sen.go.kr>
2025-05-03 20:03:03 +0900

    20250503
    노트 함수랑 기초적인 틀 만듦

--- Assets/Scripts/Note_Check.cs
+++ Assets/Scripts/Note_Check.cs
@@ -1,14 +1,117 @@
+using DG.Tweening;
 using UnityEngine;
+
+
 
 public class Note_Check : MonoBehaviour
 {
-    void Start()
+    public Transform transformPrb;
+
+    public float perfectRange = 0.2f;
+    public float goodRange = 0.5f;
+    public float missRange = 1.0f;
+
+    float Speed=5f;
+    Vector3 judgeVec;
+
+    float dist;
+    int judgeNumber;
+    private void Start()
     {
-        
+        judgeNumber = Note_Manager.inst.judgeNumber;
+        judgeVec = Note_Manager.inst.judgeLine[judgeNumber].position;
+        transformPrb = GetComponent<Transform>(); 
     }
 
     void Update()
     {
-        
+        dist = Vector2.Distance(transform.position, judgeVec);
+        if (gameObject != null)
+        {
+            transform.Translate(Vector3.up * Speed * Time.deltaTime);
+            //Debug.Log("이동");
+        }
+
+        if (judgeNumber==0&&Input.GetKeyDown(KeyCode.UpArrow)) // 방향은 각 노트에 따라 다르게
+            NoteJudge();
+        else if(judgeNumber==2&& Input.GetKeyDown(KeyCode.RightArrow))
+            NoteJudge();
+        else if (judgeNumber == 4 && Input.GetKeyDown(KeyCode.DownArrow))
+            NoteJudge();
+        else if (judgeNumber == 6 && Input.GetKeyDown(KeyCode.LeftArrow))
+            NoteJudge();
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (judgeNumber==1&& Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                NoteJudge();
+            }
+            else if(judgeNumber==7&& Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                NoteJudge();
+            }
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (judgeNumber == 3 && Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                NoteJudge();
+            }
+            else if (judgeNumber == 5 && Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                NoteJudge();
+            }
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (judgeNumber == 1 && Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                NoteJudge();
+            }
+            else if (judgeNumber == 3 && Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                NoteJudge();
+            }
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (judgeNumber == 7 && Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                NoteJudge();
+            }
+            else if (judgeNumber == 5 && Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                NoteJudge();
+            }
+        }
+    }
+
+    private void NoteJudge()
+    {
+        if (dist <= perfectRange)
+        {
+            Debug.Log("Perfect!");
+            Destroy(gameObject); // 노트 제거
+        }
+        else if (dist <= goodRange)
+        {
+            Debug.Log("Good!");
+            Destroy(gameObject); // 노트 제거
+        }
+        else if (dist <= missRange)
+        {
+            Debug.Log("Miss!");
+            Destroy(gameObject); // 노트 제거
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            Debug.Log("Miss!");
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/Note_Manager.cs
+++ Assets/Scripts/Note_Manager.cs
@@ -2,23 +2,108 @@ using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+public enum Line { Up, UpRight, Right, DownRight, Down, DownLeft, Left, UpLeft }
+
 public class Note_Manager : MonoBehaviour
 {
-    void Start()
+    public Transform[] judgeLine; // 중심 판정 라인 위치, 위에서부터 시계방향으로 01234567
+    [SerializeField] GameObject NotePrefab;
+    
+    [HideInInspector]public int judgeNumber;
+
+    public static Note_Manager inst;
+    void Awake()
     {
-        
+        inst = this;
     }
 
+    private void Start()
+    {
+        //함수 작동 Test
+        for(int i = 0;i<50;i++)
+        {
+            NoteSpawn(i, Random.Range(0, 8));
+        }
+        /*NoteSpawn(0, Line.Up);
+        NoteSpawn(2, Line.Down);
+        NoteSpawn(3, Line.Right);
+        NoteSpawn(4, Line.Left);
+        NoteSpawn(5, Line.UpLeft);
+        NoteSpawn(6, Line.DownLeft);
+        NoteSpawn(7, Line.UpRight);
+        NoteSpawn(8, Line.DownRight);     */   
+    }
     void Update()
     {
-        
+
     }
 
-    public void NoteSpawn(float Time, int Where) // Time은 어느 시간에 노트가 나올지 정하고, Where은 팔각형 중 어느 위치에서 나올지 정한다. 
+    public void NoteSpawn(float Spawntime, Line Where) //Spawntime은 생성될 시간, Where은 생성되는 노트 위치
     {
-        if (Where == 0) //팔각형의 위쪽으로, 노트의 rotate와 이동하고 내려오는 위치 등을 결정함
+        Vector3 SpawnPos = Vector3.zero;
+        DOVirtual.DelayedCall(Spawntime, () =>
         {
-            transform.rotation = new Quaternion(0,0,45,0);
-        }
+            if (Where == Line.Up || Where == Line.UpRight || Where == Line.UpLeft)
+            {
+                SpawnPos.y = 5.1f;
+                if (Where == Line.UpRight)
+                    SpawnPos.x = 5.1f;
+                if (Where == Line.UpLeft)
+                    SpawnPos.x = -5.1f;
+            }
+            else if (Where == Line.DownRight || Where == Line.Down || Where == Line.DownLeft)
+            {
+                SpawnPos.y = -5.1f;
+                if (Where == Line.DownRight)
+                    SpawnPos.x = 5.1f;
+                if (Where == Line.DownLeft)
+                    SpawnPos.x = -5.1f;
+            }
+            else
+            {
+                SpawnPos.y = 0;
+                //transform.rotation = Quaternion.Euler(0, 0, 90);
+                if (Where == Line.Right)
+                    SpawnPos.x = 5.1f;
+                if (Where == Line.Left)
+                    SpawnPos.x = -5.1f;
+            }
+            judgeNumber = (int)Where;
+            Instantiate(NotePrefab, SpawnPos, judgeLine[judgeNumber].transform.rotation);
+        });
+    }
+    public void NoteSpawn(float Spawntime, int Where) //Spawntime은 생성될 시간, Where은 생성되는 노트 위치
+    {
+        Vector3 SpawnPos = Vector3.zero;
+        DOVirtual.DelayedCall(Spawntime, () =>
+        {
+            if (Where == 0 || Where == 1 || Where == 7)
+            {
+                SpawnPos.y = 5.1f;
+                if (Where == 1)
+                    SpawnPos.x = 5.1f;
+                if (Where == 7)
+                    SpawnPos.x = -5.1f;
+            }
+            else if (Where == 3 || Where == 4 || Where == 5)
+            {
+                SpawnPos.y = -5.1f;
+                if (Where == 3)
+                    SpawnPos.x = 5.1f;
+                if (Where == 5)
+                    SpawnPos.x = -5.1f;
+            }
+            else
+            {
+                SpawnPos.y = 0;
+                //transform.rotation = Quaternion.Euler(0, 0, 90);
+                if (Where == 2)
+                    SpawnPos.x = 5.1f;
+                if (Where == 6)
+                    SpawnPos.x = -5.1f;
+            }
+            judgeNumber = Where;
+            Instantiate(NotePrefab, SpawnPos, judgeLine[judgeNumber].transform.rotation);
+        });
     }
 }
