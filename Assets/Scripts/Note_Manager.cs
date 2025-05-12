b175a83d379c72a46f38ba8560ebbc89322d40a8
ysb <vahyeon2503@sonline20.sen.go.kr>
2025-05-13 03:53:05 +0900

    노트를 음악기반으로 생성2
    20250513

--- Assets/Scripts/Note_Manager.cs
+++ Assets/Scripts/Note_Manager.cs
@@ -1,109 +1,169 @@
+using System.Collections;
 using System.Collections.Generic;
-using DG.Tweening;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public enum Line { Up, UpRight, Right, DownRight, Down, DownLeft, Left, UpLeft }
+public class NoteData
+{
+    public float hitTime; // 판정선 도달 시간 (오디오 시간 기준)
+    public Line linePosition;
+    public bool spawned = false;
+}
 
 public class Note_Manager : MonoBehaviour
 {
+    [SerializeField] private AudioSource[] audioSource;
+    int Audio=0;  // 오디오 인덱스
+    public float MusicTime => audioSource[Audio].time;
+
+    private List<NoteData> allNotes = new List<NoteData>();
+
+    bool hasTriggered, hasTriggered1 = false;
+
+    public static Note_Manager inst;
+    
     public Transform[] judgeLine; // 중심 판정 라인 위치, 위에서부터 시계방향으로 01234567
-    static public List<GameObject>[] NoteList = new List<GameObject>[7];  //노트 리스트
+    public List<GameObject>[] NoteList = new List<GameObject>[8];  //노트 리스트
+    bool[] NoteArray = new bool[125];
+    int NoteArrayNumber;
+
+    float timeToReach;
     [SerializeField] GameObject NotePrefab;
     
     [HideInInspector]public int judgeNumber;
-
-    public static Note_Manager inst;
     void Awake()
     {
         inst = this;
-    }
+        for (int i = 0; i < NoteList.Length; i++)
+        {
+            NoteList[i] = new List<GameObject>();
+        }
 
+    }
     private void Start()
     {
-        //함수 작동 Test
-        for(int i = 0;i<50;i++)
+        NoteSpawn(6.5f, Line.Right);
+        NoteSpawn(7.5f, Line.Right);
+    }
+
+    private void Update()    //update에서 if문 사용
+    {
+        //Debug.Log("Timer: "+GameManager.GameTimer);
+        //Debug.Log("MusicTime: "+ audioSource[Audio].time);
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            NoteSpawn(i, Random.Range(0, 8));
+            Debug.Log("GameTimer: "+audioSource[Audio].time);
+            Debug.Log("Key: Right");
         }
-        /*NoteSpawn(0, Line.Up);
-        NoteSpawn(2, Line.Down);
-        NoteSpawn(3, Line.Right);
-        NoteSpawn(4, Line.Left);
-        NoteSpawn(5, Line.UpLeft);
-        NoteSpawn(6, Line.DownLeft);
-        NoteSpawn(7, Line.UpRight);
-        NoteSpawn(8, Line.DownRight); */   
+        else if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log("GameTimer: " + audioSource[Audio].time);
+            Debug.Log("Key: Left");
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow)|| Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("GameTimer: " + audioSource[Audio].time);
+            Debug.Log("Key: Up and Down");
+        }
+
+        float currentTime = MusicTime;
+
+        foreach (var note in allNotes)
+        {
+            timeToReach = Note_Check.timeToReach;
+            if (!note.spawned && currentTime >= (note.hitTime - timeToReach))
+            {
+                SpawnNote(note.linePosition);
+                note.spawned = true;
+            }
+        }
     }
-    void Update()
+
+/*    public void NoteSpawn(double Spawntime, Line Where) //Spawntime은 생성될 시간, Where은 생성되는 노트 위치
     {
-
+        timeToReach = Note_Check.timeToReach;
+        NoteCoroutine((float)Spawntime, (int)Where);
     }
-
-    public void NoteSpawn(float Spawntime, Line Where) //Spawntime은 생성될 시간, Where은 생성되는 노트 위치
+    public void NoteSpawn(double Spawntime, int Where) //Spawntime은 생성될 시간, Where은 생성되는 노트 위치
+    {
+        timeToReach = Note_Check.timeToReach;
+        NoteCoroutine((float)Spawntime, Where);
+    }
+    void NoteCoroutine(float time, int Where)//딜레이를 관리함.
     {
         Vector3 SpawnPos = Vector3.zero;
-        DOVirtual.DelayedCall(Spawntime, () =>
+        //yield return new WaitForSeconds(time- timeToReach);
+        if (Where == 0 || Where == 1 || Where == 7)
         {
-            if (Where == Line.Up || Where == Line.UpRight || Where == Line.UpLeft)
-            {
-                SpawnPos.y = 5.1f;
-                if (Where == Line.UpRight)
-                    SpawnPos.x = 5.1f;
-                if (Where == Line.UpLeft)
-                    SpawnPos.x = -5.1f;
-            }
-            else if (Where == Line.DownRight || Where == Line.Down || Where == Line.DownLeft)
-            {
-                SpawnPos.y = -5.1f;
-                if (Where == Line.DownRight)
-                    SpawnPos.x = 5.1f;
-                if (Where == Line.DownLeft)
-                    SpawnPos.x = -5.1f;
-            }
-            else
-            {
-                SpawnPos.y = 0;
-                if (Where == Line.Right)
-                    SpawnPos.x = 5.1f;
-                if (Where == Line.Left)
-                    SpawnPos.x = -5.1f;
-            }
-            judgeNumber = (int)Where;
-            Instantiate(NotePrefab, SpawnPos, judgeLine[judgeNumber].transform.rotation);
-        });
+            SpawnPos.y = 5.1f;
+            if (Where == 1)
+                SpawnPos.x = 5.1f;
+            if (Where == 7)
+                SpawnPos.x = -5.1f;
+        }
+        else if (Where == 3 || Where == 4 || Where == 5)
+        {
+            SpawnPos.y = -5.1f;
+            if (Where == 3)
+                SpawnPos.x = 5.1f;
+            if (Where == 5)
+                SpawnPos.x = -5.1f;
+        }
+        else
+        {
+            SpawnPos.y = 0;
+            if (Where == 2)
+                SpawnPos.x = 5.1f;
+            if (Where == 6)
+                SpawnPos.x = -5.1f;
+        }
+        judgeNumber = Where;
+        Instantiate(NotePrefab, SpawnPos, judgeLine[judgeNumber].transform.rotation);
+    }*/
+    private void SpawnNote(Line line)
+    {
+        int where = (int)line;
+
+        Vector3 spawnPos = GetSpawnPosition(where);
+        GameObject note = Instantiate(NotePrefab, spawnPos, judgeLine[where].rotation);
+        judgeNumber = where;
+
+        NoteList[where].Add(note);
     }
-    public void NoteSpawn(float Spawntime, int Where) //Spawntime은 생성될 시간, Where은 생성되는 노트 위치
+    private Vector3 GetSpawnPosition(int where)
     {
         Vector3 SpawnPos = Vector3.zero;
-        DOVirtual.DelayedCall(Spawntime, () =>
+
+        if (where == 0 || where == 1 || where == 7)
         {
-            if (Where == 0 || Where == 1 || Where == 7)
-            {
-                SpawnPos.y = 5.1f;
-                if (Where == 1)
-                    SpawnPos.x = 5.1f;
-                if (Where == 7)
-                    SpawnPos.x = -5.1f;
-            }
-            else if (Where == 3 || Where == 4 || Where == 5)
-            {
-                SpawnPos.y = -5.1f;
-                if (Where == 3)
-                    SpawnPos.x = 5.1f;
-                if (Where == 5)
-                    SpawnPos.x = -5.1f;
-            }
-            else
-            {
-                SpawnPos.y = 0;
-                if (Where == 2)
-                    SpawnPos.x = 5.1f;
-                if (Where == 6)
-                    SpawnPos.x = -5.1f;
-            }
-            judgeNumber = Where;
-            Instantiate(NotePrefab, SpawnPos, judgeLine[judgeNumber].transform.rotation);
-        });
+            SpawnPos.y = 5.1f;
+            if (where == 1)
+                SpawnPos.x = 5.1f;
+            if (where == 7)
+                SpawnPos.x = -5.1f;
+        }
+        else if (where == 3 || where == 4 || where == 5)
+        {
+            SpawnPos.y = -5.1f;
+            if (where == 3)
+                SpawnPos.x = 5.1f;
+            if (where == 5)
+                SpawnPos.x = -5.1f;
+        }
+        else
+        {
+            SpawnPos.y = 0;
+            if (where == 2)
+                SpawnPos.x = 5.1f;
+            if (where == 6)
+                SpawnPos.x = -5.1f;
+        }
+
+        return SpawnPos;
+    }
+    void NoteSpawn(float time, Line line)
+    {
+        allNotes.Add(new NoteData { hitTime = time, linePosition = line });
     }
 }
